8a4b4f5d74de6fdd61c714ef3a170783fa85c087
Артем <128614819+artemmovish@users.noreply.github.com>
2025-05-24 13:41:37 +0300

    Добавьте файлы проекта.

---
+++ Vosk/Program.cs
@@ -0,0 +1,31 @@
+﻿using Vosk;
+using NAudio.Wave;
+using System;
+using Microsoft.VisualBasic;
+using System.Reflection;
+
+class Program
+{
+    static void Main()
+    {
+        Vosk.Vosk.SetLogLevel(0); // Отключает логгирование
+        Model model = new Model("models/vosk-model-small-ru-0.22");
+        using var waveIn = new WaveInEvent();
+        waveIn.WaveFormat = new WaveFormat(16000, 1);
+        var recognizer = new VoskRecognizer(model, 16000.0f);
+
+        waveIn.DataAvailable += (s, a) =>
+        {
+            if (recognizer.AcceptWaveform(a.Buffer, a.BytesRecorded))
+                Console.WriteLine(recognizer.Result());
+            else
+                Console.WriteLine(recognizer.PartialResult());
+        };
+
+        waveIn.StartRecording();
+        Console.WriteLine("Говорите... (нажмите любую клавишу для завершения)");
+        Console.ReadKey();
+        waveIn.StopRecording();
+        Console.WriteLine(recognizer.FinalResult());
+    }
+}
---
+++ VoskTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using Vosk;
+using NAudio.Wave;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string modelPath = @"E:\Project\Trainee\C#\PeerTube-Subscribe\VoskTest\vosk-model-en-us-0.22-lgraph\";
+        string audioFilePath = @"E:\Project\Trainee\C#\PeerTube-Subscribe\VoskTest\Sounds\male.wav";
+
+        Vosk.Vosk.SetLogLevel(0);
+        Model model = new Model(modelPath);
+
+        using (var waveReader = new WaveFileReader(audioFilePath))
+        {
+            // Проверяем исходный формат
+            Console.WriteLine($"Исходный формат: {waveReader.WaveFormat.SampleRate} Hz, {waveReader.WaveFormat.Channels} каналов");
+
+            // Если частота не 16 кГц, ресемплируем
+            var targetFormat = new WaveFormat(16000, 1); // 16 кГц, моно
+            using (var resampler = new MediaFoundationResampler(waveReader, targetFormat))
+            {
+                var recognizer = new VoskRecognizer(model, 16000.0f);
+
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+
+                while ((bytesRead = resampler.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    if (recognizer.AcceptWaveform(buffer, bytesRead))
+                    {
+                        Console.WriteLine(recognizer.Result());
+                    }
+                    else
+                    {
+                        Console.WriteLine(recognizer.PartialResult());
+                    }
+                }
+
+                Console.WriteLine(recognizer.FinalResult());
+            }
+        }
+    }
+}
